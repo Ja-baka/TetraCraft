4f07bd08013bbc84e0bac5f27b4506bb363052e0
Ja-baka <73071447+Ja-baka@users.noreply.github.com>
2022-04-12 10:05:57 +0300

    Зададзены каардзінаты дзеля кожнай фігуры

--- Assets/TetraCraft/Scripts/Model/Shapes/ShapeL.cs
+++ Assets/TetraCraft/Scripts/Model/Shapes/ShapeL.cs
@@ -4,21 +4,69 @@ internal class ShapeL : Shape
 {
     protected override void CreateRotate1(out Vector2Int[] rotate)
     {
-        throw new System.NotImplementedException();
+        rotate = new Vector2Int[4];
+
+        rotate[0].x = 0;
+        rotate[0].y = 2;
+
+        rotate[1].x = 1;
+        rotate[1].y = 2;
+
+        rotate[2].x = 2;
+        rotate[2].y = 2;
+
+        rotate[3].x = 2;
+        rotate[3].y = 3;
     }
 
     protected override void CreateRotate2(out Vector2Int[] rotate)
     {
-        throw new System.NotImplementedException();
+        rotate = new Vector2Int[4];
+
+        rotate[0].x = 1;
+        rotate[0].y = 3;
+
+        rotate[1].x = 1;
+        rotate[1].y = 2;
+
+        rotate[2].x = 1;
+        rotate[2].y = 1;
+
+        rotate[3].x = 2;
+        rotate[3].y = 1;
     }
 
     protected override void CreateRotate3(out Vector2Int[] rotate)
     {
-        throw new System.NotImplementedException();
+        rotate = new Vector2Int[4];
+
+        rotate[0].x = 0;
+        rotate[0].y = 1;
+
+        rotate[1].x = 0;
+        rotate[1].y = 2;
+
+        rotate[2].x = 1;
+        rotate[2].y = 2;
+
+        rotate[3].x = 2;
+        rotate[3].y = 2;
     }
 
     protected override void CreateRotate4(out Vector2Int[] rotate)
     {
-        throw new System.NotImplementedException();
+        rotate = new Vector2Int[4];
+
+        rotate[0].x = 0;
+        rotate[0].y = 3;
+
+        rotate[1].x = 1;
+        rotate[1].y = 3;
+
+        rotate[2].x = 1;
+        rotate[2].y = 2;
+
+        rotate[3].x = 1;
+        rotate[3].y = 1;
     }
 }
