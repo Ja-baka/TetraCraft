675bedfb5441f3242da7923c9b86842c150adc08
Ja-baka <73071447+Ja-baka@users.noreply.github.com>
2022-04-14 15:45:08 +0300

    Дадана лёгіка руху фігуры

--- Assets/TetraCraft/Scripts/Field.cs
+++ Assets/TetraCraft/Scripts/Field.cs
@@ -5,12 +5,12 @@ public class Field : MonoBehaviour
 {
     [SerializeField] private Spawner _spawner;
     [SerializeField] private ActiveTetramino _tetramino;
-    private BlockMaterial[,] _blocks;
+    private BlockMaterial[,] _cells;
     private List<Vector2Int> _tetraminoPosition;
 
     private void Start()
     {
-        _blocks = FillArray();
+        _cells = FillArray();
         _tetraminoPosition = new List<Vector2Int>();
     }
 
@@ -20,6 +20,7 @@ public class Field : MonoBehaviour
         const int ClassicTetrisFieldHeigth = 20;
         BlockMaterial[,] array = new BlockMaterial
             [ClassicTetrisFieldWidth, ClassicTetrisFieldHeigth];
+
         for (int i = 0; i < array.GetLength(0); i++)
         {
             for (int j = 0; j < array.GetLength(1); j++)
@@ -53,24 +54,24 @@ public class Field : MonoBehaviour
             int x = block.Position.x;
             int y = block.Position.y;
 
-            if ((_blocks[x, y] is Air) == false)
+            if ((_cells[x, y] is Air) == false)
             {
                 GameOver();
             }
 
-            _blocks[x, y] = block.Material;
+            _cells[x, y] = block.Material;
             _tetraminoPosition.Add(block.Position);
         }
     }
 
     private void OnTetraminoFalled()
     {
-        for (int i = 0; i < _blocks.GetLength(0); i++)
+        for (int i = 0; i < _cells.GetLength(0); i++)
         {
             bool isFullRow = false;
-            for (int j = 0; j < _blocks.GetLength(1); j++)
+            for (int j = 0; j < _cells.GetLength(1); j++)
             {
-                if ((_blocks[i, j] is Air) == false)
+                if ((_cells[i, j] is Air) == false)
                 {
                     isFullRow = true;
                 }
@@ -82,11 +83,20 @@ public class Field : MonoBehaviour
         }
     }
 
-    private void OnTetraminoMoved(Vector2Int newPosition)
+    private void OnTetraminoMoved(Vector2Int offset)
+    {
+        UpdateTetraminoPosition(Air.Instance);
+
+        _tetraminoPosition.ForEach((p) => p += offset);
+
+        UpdateTetraminoPosition(_tetramino.Material);
+    }
+
+    private void UpdateTetraminoPosition(BlockMaterial material)
     {
         foreach (Vector2Int oldPosition in _tetraminoPosition)
         {
-            _blocks[oldPosition.x, oldPosition.y] = Air.Instance;
+            _cells[oldPosition.x, oldPosition.y] = material;
         }
     }
 
