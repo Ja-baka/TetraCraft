06ae408ad58e4bc8d9a36ba18cb84e9250ff8bec
Ja-baka <73071447+Ja-baka@users.noreply.github.com>
2022-05-07 10:23:12 +0300

    Блёк пяска валіцца да канца

--- Assets/TetraCraft/Scripts/Materials/Properties/Falling.cs
+++ Assets/TetraCraft/Scripts/Materials/Properties/Falling.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(menuName = "SO/BlockMaterial/Properties/Weight/Falling", fileName = "Falling")]
 public class Falling : ScriptableObject, IWeight
@@ -7,14 +6,15 @@ public class Falling : ScriptableObject, IWeight
     public void Fall(Vector2Int current, ref BlockMaterial[,] field)
     {
         Vector2Int bellow = current + Vector2Int.down;
-        if (bellow.y < 0
-            || field[bellow.x, bellow.y] != null)
+        while (bellow.y >= 0
+            && field[bellow.x, bellow.y] == null)
         {
-            return;
-        }
+            field[bellow.x, bellow.y] = field[current.x, current.y];
+            field[current.x, current.y] = null;
 
-        field[bellow.x, bellow.y] = field[current.x, current.y];
-        field[current.x, current.y] = null;
+            bellow += Vector2Int.down;
+            current += Vector2Int.down;
+        }
     }
 }
 
